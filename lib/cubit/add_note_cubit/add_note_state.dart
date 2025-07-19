8b5d354c34568668f22a3699c6b4af7df7ec5a5f
MohamedFares09 <mofares010@gmail.com>
2025-07-20 01:31:55 +0300

    create add note cubit and state

---
+++ lib/cubit/add_note_cubit/add_note_state.dart
@@ -0,0 +1,5 @@
+class AddNoteState {}
+class AddNoteInit extends AddNoteState{}
+class AddNoteLoading extends AddNoteState{}
+class AddNoteSuccess extends AddNoteState{}
+class AddNoteFaileur extends AddNoteState {}
